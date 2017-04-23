960b879909caca51717c268f122fb06b8e3cf011
andrewstart <andrewstart@cloudkid.com>
2017-04-22 23:03:15 -0400

    Added mixin for BaseTexture.destroy()

--- dist/pixi-display.js
+++ dist/pixi-display.js
@@ -7,6 +7,47 @@
 (function(undefined)
 {
 	/**
+	 *  Mixins for the PIXI BaseTexture class
+	 *  @class BaseTexture
+	 */
+
+	var BaseTexture = include("PIXI.BaseTexture", false);
+	if (!BaseTexture) return;
+
+	var p = BaseTexture.prototype;
+
+	var orig_destroy = p.destroy;
+	p.destroy = function()
+	{
+		orig_destroy.call(this);
+
+		//go through and destroy any textures that use this as the base texture
+		//that way a destroyed spritesheet cleans up all the sprite frames
+		var TextureCache = PIXI.utils.TextureCache;
+		for(var id in TextureCache)
+		{
+			var texture = TextureCache[id];
+			if(!texture)
+			{
+				delete TextureCache[id];
+				continue;
+			}
+			if(texture.baseTexture === this)
+			{
+				texture.destroy();
+			}
+		}
+	};
+
+}());
+/**
+ * @module PIXI Display
+ * @namespace PIXI
+ * @requires Core
+ */
+(function(undefined)
+{
+	/**
 	 *  Mixins for the PIXI BitmapText class
 	 *  @class BitmapText
 	 */
